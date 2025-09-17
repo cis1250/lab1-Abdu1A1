c8df8c535a0595fbe048749d82fd36e4c1df50fb
Abdul Ahad <ahada@uoguelph.ca>
2025-09-17 12:07:41 -0400

    hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@
 # 1. Keep "Hello, world!" as the first line.
 # 2. Add another print statement with your name.
 # 3. Add another print statement with your favorite hobby.
-
+print("Hello, world!")
 print("Hello, Abdul Ahad")
 print("I play basketball")
 
